52b51683b9f7f9e0d70b22b74adf42ad8634bd6e
PRASHANT <PRASHANT@192.168.1.97>
2026-04-18 17:11:56 +0530

    practice commit

---
+++ src/operators/test.java
@@ -0,0 +1,12 @@
+package operators;
+
+public class test {
+public static void main(String[] args) {
+	
+
+System.out.println(1+1);
+System.out.println(12-3);
+System.out.println(2*3);
+System.out.println(12/3);
+}
+}
