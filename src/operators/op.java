6c4ad2c2ebbcfeddde2443c96f25b7a32d19c5e5
PRASHANT <PRASHANT@prashant>
2026-04-18 17:51:07 +0530

    my practice commit implemented

---
+++ src/operators/op.java
@@ -0,0 +1,12 @@
+package operators;
+
+public class op {
+	public static void main(String[] args) {
+		System.out.println(14125+151523);
+		System.out.println(3252352-35235);
+		System.out.println(5236*3463);
+		System.out.println(23462346/34266236);
+
+	}
+
+}
