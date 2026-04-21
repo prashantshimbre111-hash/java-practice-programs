d2f709e75e89178f4e374c0942d1924fbe746860
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-04-21 12:16:14 +0530

    implement returntype non-parameterized method

---
+++ src/returntypemethod/Add.java
@@ -0,0 +1,16 @@
+package returntypemethod;
+
+public class Add { 
+	
+	public int calculation() {
+	
+	int a = 445;
+	int b = 665;
+	int  add = a+b;
+	return add;
+	
+	
+	
+	
+	}
+}
---
+++ src/returntypemethod/Test.java
@@ -0,0 +1,13 @@
+package returntypemethod;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		Add a = new Add();
+		int total = a.calculation();
+		System.out.println(total);
+
+	}
+
+}
