db98f05cf63fe077ce01d9735801686062cee12c
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-04-21 11:33:52 +0530

    arithematic operators commit implemented

---
+++ src/operators/practice.java
@@ -0,0 +1,18 @@
+package operators;
+
+public class practice {
+	public static void main(String[] args) {
+		
+		int c = 333;
+		int d = 54;
+		
+		System.out.println("additon: " + c+d);
+		System.out.println("subtraction : " + (c-d));
+		System.out.println("multiplication : " + c*d);
+		System.out.println("division : " + c/d);
+		System.out.println("multiplication divide by division : " + (c*d)/(c/d));
+	}
+	
+	
+
+}
