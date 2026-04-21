a76e6579853ad644ac28d47ef69bd712b7a10172
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-04-21 12:22:08 +0530

    Implement void parameterized method

---
+++ src/voidparameterizedmethod/Add.java
@@ -0,0 +1,15 @@
+package voidparameterizedmethod;
+
+public class Add {
+	
+	public void add(int a , int b) {
+
+		
+		int total = a+b;
+		System.out.println("addtion : " + total);
+		
+		
+		
+	}
+
+}
---
+++ src/voidparameterizedmethod/Test.java
@@ -0,0 +1,10 @@
+package voidparameterizedmethod;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		Add a = new Add();
+		a.add(23 , 54);
+	}
+}
