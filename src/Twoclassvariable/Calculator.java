51cd73801ad0205cada83dbca0e530c55967fcfb
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-04-22 13:49:20 +0530

    implement Calculator subtraction by using Twoclass variable

---
+++ src/Twoclassvariable/Calculator.java
@@ -0,0 +1,21 @@
+package Twoclassvariable;
+
+import java.util.Scanner;
+
+public class Calculator {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("enter value of a");
+		int a = sc.nextInt();
+		
+		System.out.println("enter value of b");
+		int b = sc.nextInt();
+		
+		int total = a+b;
+		System.out.println("adition of a and b : " + (a+b));
+	}
+
+}
--- src/Twoclassvariable/Subcal.java
+++ src/Twoclassvariable/Subcal.java
@@ -1,21 +1,22 @@
-package operatorsTwoclassvariable;
+package Twoclassvariable;
 
 import java.util.Scanner;
 
-public class Calculator {
-	
+public class Subcal {
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.println("enter value of a");
 		int a = sc.nextInt();
-		
+
 		System.out.println("enter value of b");
 		int b = sc.nextInt();
+
+		int sub = (a - b);
+		System.out.println("a-b : " + sub);
 		
-		int total = a+b;
-		System.out.println("adition of a and b : " + (a+b));
+		
 	}
 
 }
