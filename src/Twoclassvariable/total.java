1bc9166824a3170adfd9b799d033d79987ec03ba
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-04-22 15:32:34 +0530

    implement addition with Twoclass

---
+++ src/Twoclassvariable/total.java
@@ -0,0 +1,22 @@
+package Twoclassvariable;
+
+import java.util.Scanner;
+
+public class total {
+	
+	public static void main(String[] args) {
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("please enter a");
+		 int a = sc.nextInt();
+		 System.out.println("pleae enter b");
+		 int b = sc.nextInt();
+		 
+		 int total = a+b;
+		 
+		 System.out.println("a+b : " + total);
+	}
+
+}
