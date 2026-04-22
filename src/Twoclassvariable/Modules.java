e7bf41941426f0b0a61e7b99727c89576ce3cea7
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-04-22 15:45:56 +0530

    implemented modules operator with Twoclass

---
+++ src/Twoclassvariable/Modules.java
@@ -0,0 +1,21 @@
+package Twoclassvariable;
+
+import java.util.Scanner;
+
+public class Modules {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("pleae enter a : ");
+		int a = sc.nextInt();
+		
+		System.out.println("please enter b : ");
+		int b = sc.nextInt();
+		
+		int mod = a%b;
+		System.out.println("mod of a and b : " + mod);
+		
+	}
+
+}
