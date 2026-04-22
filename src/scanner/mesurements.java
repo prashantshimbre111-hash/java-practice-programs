79286d7aae6892a948bc58a35820716b535aee0d
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-04-22 17:33:27 +0530

    Implement all arithematic operator with scanner

--- src/scanner/mesurements.java
+++ src/scanner/mesurements.java
@@ -6,12 +6,27 @@ public class mesurements {
 
 	public static void main(String[] args) {
 
-	Scanner sc = new Scanner(System.in);
-	
-	int a = sc.nextInt();
-	int b = sc.nextInt();
-	int total = a+b;
-	System.out.println("total of a and b : " + total);
+		Scanner sc = new Scanner(System.in);
 
-}
+		System.out.println("please enter a :");
+		int a = sc.nextInt();
+		System.out.println("please enter b :");
+		int b = sc.nextInt();
+		int addition = a + b;
+		System.out.println("Addition of a and b : " + addition);
+		
+		int subtraction = a-b;
+		System.out.println("Subtraction of a and b : " + subtraction);
+		
+		int multiplication = a*b;
+		System.out.println("multiplication of a and b : " + multiplication);
+		
+		int division = a/b;
+		System.out.println("division of a and b : " + division);
+		
+		int modules = a%b;
+		System.out.println("mod of a and b : " + modules);
+		
+
+	}
 }
