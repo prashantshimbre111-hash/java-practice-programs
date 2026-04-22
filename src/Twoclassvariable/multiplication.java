e4aee7b30d7615d8dd01576edb7a7ab46c81af50
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-04-22 15:36:33 +0530

    implement multiplication with Twoclass

---
+++ src/Twoclassvariable/multiplication.java
@@ -0,0 +1,22 @@
+package Twoclassvariable;
+
+import java.util.Scanner;
+
+public class multiplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("please enter a");
+		int a = sc.nextInt();
+		System.out.println("pleae enter b");
+		int b = sc.nextInt();
+		
+		int mul = a*b;
+		System.out.println("multiplication of a and b : " + mul);
+
+	}
+
+}
