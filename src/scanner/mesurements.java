4b25138b29a22b66489783ad5e06ad050b43bc05
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-04-22 16:51:52 +0530

    Implement measurements with scanner

---
+++ src/scanner/mesurements.java
@@ -0,0 +1,17 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class mesurements {
+
+	public static void main(String[] args) {
+
+	Scanner sc = new Scanner(System.in);
+	
+	int a = sc.nextInt();
+	int b = sc.nextInt();
+	int total = a+b;
+	System.out.println("total of a and b : " + total);
+
+}
+}
