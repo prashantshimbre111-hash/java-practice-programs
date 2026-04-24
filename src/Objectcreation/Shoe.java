76f1a3c5f438091174ecae4892c80b4f4acdb809
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-04-24 16:43:30 +0530

    Object creation implemented

---
+++ src/Objectcreation/Shoe.java
@@ -0,0 +1,10 @@
+package Objectcreation;
+
+public class Shoe {
+	
+	String brandname = "redtape";
+	String colour = "white with green lines";
+	int price = 1340;
+	
+
+}
---
+++ src/Objectcreation/flipkart.java
@@ -0,0 +1,16 @@
+package Objectcreation;
+
+public class flipkart {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Shoe s = new Shoe();
+		
+		System.out.println("brandname : " + s.brandname);
+		System.out.println("colour : " + s.colour);
+		System.out.println("price : " + s.price);
+
+	}
+
+}
