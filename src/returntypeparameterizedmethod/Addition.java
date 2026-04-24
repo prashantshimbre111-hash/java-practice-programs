1bb06429fdbdcc0ca1130e523dc585afc922531f
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-04-24 17:08:14 +0530

    Implement returntype parameterized method example

---
+++ src/returntypeparameterizedmethod/Addition.java
@@ -0,0 +1,11 @@
+package returntypeparameterizedmethod;
+
+public class Addition {
+	
+	public int sum(int p , int q) {
+		
+		int sum = p+q;
+		return sum;
+	}
+
+}
--- src/returntypeparameterizedmethod/Test.java
+++ src/returntypeparameterizedmethod/Test.java
@@ -7,6 +7,12 @@ public class Test {
 		Add a = new Add();
 		int value = a.cal(21 , 65);
 		System.out.println("addition with return parameterized : " + value);
+		
+		Addition A = new Addition();
+		int cal = A.sum(32, 43);
+		System.out.println("addition of p and q by using returntype parameterized method : " + cal);
+		
+		
 	}
 
 }
