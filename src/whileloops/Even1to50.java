fbcf9bccbc186e0113f9ef566795dbc9a9e76c64
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-02 14:51:43 +0530

    Print even numbers from 1 to 50.

---
+++ src/whileloops/Even1to50.java
@@ -0,0 +1,19 @@
+package whileloops;
+
+public class Even1to50 {
+
+	public static void main(String[] args) {
+		
+//		3. Print even numbers from 1 to 50.
+		System.out.println("below list is even number from 1 to 50 : ");
+		int i = 0;
+		while(i<=50) {
+			
+			System.out.println(i);
+			
+			i+=2;
+		}
+		
+	}
+
+}
