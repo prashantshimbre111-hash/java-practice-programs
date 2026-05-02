bc47cb62aabd9b0fdd4f178fd55f3cfc2c151971
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-02 14:54:50 +0530

    Print odd numbers from 1 to 50 implemented.

---
+++ src/whileloops/Odd1to50.java
@@ -0,0 +1,19 @@
+package whileloops;
+
+public class Odd1to50 {
+	
+	public static void main(String[] args) {
+//		Print odd numbers from 1 to 50.
+		
+		System.out.println("below list is odd number from 1 to 50 : ");
+		
+		int i = 1;
+		while(i<=50) {
+			
+			System.out.println(i);
+			
+			i+=2;
+		}
+	}
+
+}
