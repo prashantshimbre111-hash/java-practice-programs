dd0d1bcb9e08c6e88e17ee1be4cd5d127f5fa10d
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-02 14:47:17 +0530

    Print numbers from 10 to one using a while loop.

---
+++ src/whileloops/Int10to1.java
@@ -0,0 +1,18 @@
+package whileloops;
+
+public class Int10to1 {
+	
+	public static void main(String[] args) {
+//		Print numbers from 10 to one using a while loop.
+		
+		int i = 10;
+		while(i>0) {
+			System.out.println(i);
+			
+			i--;
+		}
+		}
+		
+	}
+
+
