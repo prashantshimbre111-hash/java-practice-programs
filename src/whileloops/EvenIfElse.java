187228a6b9835f46e82c16d6759461d28e915d74
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-02 15:00:00 +0530

    Print even numbers from 1 to 50 with if else condition implemented

---
+++ src/whileloops/EvenIfElse.java
@@ -0,0 +1,16 @@
+package whileloops;
+
+public class EvenIfElse {
+	public static void main(String[] args) {
+//		Print even numbers from 1 to 50 with if else condition.
+		System.out.println("below list is even numbers from 1 to 50 :");
+		int i = 1;
+		while(i<=50) {
+			
+			if(i%2==0) {
+				System.out.println(i);
+			}
+		i++;	
+	}
+
+}}
