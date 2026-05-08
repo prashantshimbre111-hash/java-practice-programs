8f5ba3e533eea2a09192d45f7817af008f4e5780
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-08 11:05:41 +0530

    Constructor Practice question implemented.

---
+++ src/constructor/question9/Exkingdom.java
@@ -0,0 +1,14 @@
+package constructor.question9;
+
+public class Exkingdom {
+	
+	public Exkingdom(String name) {
+		
+		Ex e = new Ex();
+		e.printname(name);
+		
+		
+	}
+	
+
+}
---
+++ src/constructor/question9/Test.java
@@ -0,0 +1,10 @@
+package constructor.question9;
+
+public class Test {
+	
+	public static  void main(String[]args) {
+		
+		Exkingdom ex = new Exkingdom("lion");
+	}
+
+}
