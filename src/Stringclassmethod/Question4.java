c0a162e15aa91b00276efab2feeb1cd1eb2ae6ed
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-08 15:56:10 +0530

    String class method question 2 implemented.

---
+++ src/Stringclassmethod/Question4.java
@@ -0,0 +1,10 @@
+package Stringclassmethod;
+
+public class Question4 {
+
+		public static void main(String[] args) {
+			
+			String str = "Hello World";
+			System.out.println(str.equalsIgnoreCase("hello world"));
+		}
+}
