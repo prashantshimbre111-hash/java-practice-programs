3c630762d56dad00f63aef937da5b06a104403e9
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-08 16:39:38 +0530

    String class method question implemented.

---
+++ src/Stringclassmethod/Question17.java
@@ -0,0 +1,11 @@
+package Stringclassmethod;
+
+public class Question17 {
+	public static void main(String[] args) {
+		
+		String str = "Test";
+		System.out.println(str.toLowerCase());
+		System.out.println(str.toUpperCase());
+	}
+
+}
