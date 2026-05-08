5e49c8adc9d8903b2c2c7e7ccbe989bde4664dd8
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-08 13:20:49 +0530

    Methods of string implemented.

---
+++ src/String/methodsofString.java
@@ -0,0 +1,18 @@
+package String;
+
+public class methodsofString {
+
+	public static void main(String[] args) {
+
+		String a = "The man comapny";
+
+		System.out.println(a);
+		System.out.println(a.length());
+		System.out.println(a.concat(" vitamin c serum"));
+		System.out.println(a.charAt(8));
+		System.out.println(a.toLowerCase());
+		System.out.println(a.toUpperCase());
+		System.out.println(a.contains("m"));
+
+	}
+}
