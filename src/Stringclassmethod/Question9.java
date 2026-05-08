14db9c1502427cec1651cf8088a982e436bf4571
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-08 16:09:29 +0530

    String class method question implemented.

---
+++ src/Stringclassmethod/Question9.java
@@ -0,0 +1,14 @@
+package Stringclassmethod;
+
+public class Question9 {
+
+		public static void main(String[] args) {
+			
+			String a = "apple";
+			String b = "banana";
+			
+			System.out.println(a.compareTo(b));
+			System.out.println("apple".compareTo("banana"));
+		}
+
+}
