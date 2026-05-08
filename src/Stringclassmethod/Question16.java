8026527b5638e03c73225a9ee2d1e7e88a1173a6
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-08 16:37:35 +0530

    String class method question implemented.

---
+++ src/Stringclassmethod/Question16.java
@@ -0,0 +1,12 @@
+package Stringclassmethod;
+
+public class Question16 {
+	public static void main(String[] args) {
+		
+		String s = "";
+		String v = " ";
+		System.out.println(s.isEmpty());
+		System.out.println(v.isEmpty());
+	}
+
+}
