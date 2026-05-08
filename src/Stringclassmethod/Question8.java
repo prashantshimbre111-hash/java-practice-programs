0539a25ef563318c9ab262c0187ca983c89add35
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-08 16:05:36 +0530

    String class method question implemented.

---
+++ src/Stringclassmethod/Question8.java
@@ -0,0 +1,12 @@
+package Stringclassmethod;
+
+public class Question8 {
+	
+	public static void main(String[] args) {
+		
+		String str = "programming";
+		
+		System.out.println(str.substring(3 , 6));
+	}
+
+}
