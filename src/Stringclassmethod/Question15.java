49b0e27b0f2e00ba9e75aba2c45b6b650ed610f3
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-08 16:33:08 +0530

    String class method question implemented.

---
+++ src/Stringclassmethod/Question15.java
@@ -0,0 +1,10 @@
+package Stringclassmethod;
+
+public class Question15 {
+
+		public static void main(String[] args) {
+			
+			String s = "testubebaby";
+			System.out.println(s.contains("test"));
+		}
+}
