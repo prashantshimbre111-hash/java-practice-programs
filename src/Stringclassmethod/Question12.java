54f44bfd3ebe2c15eb34a30b940c499336a18a98
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-08 16:23:30 +0530

    String class method question implemented.

---
+++ src/Stringclassmethod/Question12.java
@@ -0,0 +1,11 @@
+package Stringclassmethod;
+
+public class Question12 {
+	public static void main(String[] args) {
+		
+		String str = "abrakadabra";
+		
+		System.out.println(str.lastIndexOf('a'));
+	}
+
+}
