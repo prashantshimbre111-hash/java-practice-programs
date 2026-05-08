e54adce658deb48f86dffc5aa375dcb85096a34f
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-08 16:47:14 +0530

    String class method question implemented.

---
+++ src/Stringclassmethod/Question19.java
@@ -0,0 +1,12 @@
+package Stringclassmethod;
+
+public class Question19 {
+	public static void main(String[] args) {
+		
+		String a = "Hello";
+		String b = "Hello";
+		
+		System.out.println(a.equals(b));
+	}
+
+}
