e72901e4c2052e797fb7d653321da3aac6dfc565
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-08 16:02:58 +0530

    String class method question 2 implemented.

---
+++ src/Stringclassmethod/Question7.java
@@ -0,0 +1,12 @@
+package Stringclassmethod;
+
+public class Question7 {
+	
+	public static void main(String[] args) {
+		
+		String str = "Hello World";
+		
+		System.out.println(str.substring(5));
+	}
+
+}
