f4bed8dfd5a73b9bcb1b38427f2d835632f30611
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-08 16:31:08 +0530

    String class method question implemented.

---
+++ src/Stringclassmethod/Question14.java
@@ -0,0 +1,11 @@
+package Stringclassmethod;
+
+public class Question14 {
+	public static void main(String[] args) {
+		
+		String format = "abc";
+		
+		System.out.println(format.replace('b' , 'x'));
+	}
+
+}
