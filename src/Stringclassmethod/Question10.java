f8c8831a45e46f7b7c24a7335839fe4f7601225a
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-08 16:13:12 +0530

    String class method question implemented.

---
+++ src/Stringclassmethod/Question10.java
@@ -0,0 +1,11 @@
+package Stringclassmethod;
+
+public class Question10 {
+
+	public static void main(String[] args) {
+		
+		String a = "precautons";
+		
+		System.out.println(a.startsWith("pre"));
+	}
+}
