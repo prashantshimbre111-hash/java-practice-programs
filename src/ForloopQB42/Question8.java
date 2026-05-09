0c6e0f6378387f89476a8dc618812e97cb2c94d7
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-09 14:22:19 +0530

    Print numbers from 1 to n but skip multiples of 5.

--- src/ForloopQB42/Question8.java
+++ src/ForloopQB42/Question8.java
@@ -1,10 +1,26 @@
 package ForloopQB42;
 
+import java.util.Scanner;
+
 public class Question8 {
 	
 	public static void main(String[] args) {
 		
+		Scanner sc  =new Scanner(System.in);
 		
+		System.out.println("please enter number ");
+		int n = sc.nextInt();
+		
+		for(int i = 1 ; i <= n ; i++) {
+			
+			if(i % 5 == 0) {
+				
+				continue;
+				
+			}
+		
+		System.out.println(i);
+		}
 	
 	}
 
