d5aa8e3fdf32ca710da2515f3a5bac273df7512b
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-09 12:44:49 +0530

    4.2 QB - loops in java [for loop Advanced]

---
+++ src/ForloopQB42/Question4.java
@@ -0,0 +1,14 @@
+package ForloopQB42;
+
+public class Question4 {
+	public static void main(String[] args) {
+		
+		for(int i = 50 ; i > 0 ; i--) {
+			
+			if(i%2==1) {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
---
+++ src/ForloopQB42/Question5.java
@@ -0,0 +1,17 @@
+package ForloopQB42;
+
+import java.util.Scanner;
+
+public class Question5 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("please enter valid number");
+		int n = sc.nextInt();
+		
+		for(int i = 1 ; i <= n ; i++ ) {
+			System.out.println(i*i);
+			
+	}
+
+	}}
