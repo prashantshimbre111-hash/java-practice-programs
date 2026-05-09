cc24ba3a55b7af403157131f2455bc5415f41b4d
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-09 16:22:38 +0530

    5.2 QB  String using for loop

---
+++ src/Stringusingforloop/Question3.java
@@ -0,0 +1,22 @@
+package Stringusingforloop;
+
+public class Question3 {
+	
+	public static void main(String[] args) {
+		
+			String str = "prashantshimbare";
+
+			
+			int count = 0;
+			
+			for(int i = 0 ; i < str.length() ; i++) {
+				 char cha = str.charAt(i);
+				 
+				 count++;
+				
+			}
+			System.out.println(count);
+
+	}
+
+}
