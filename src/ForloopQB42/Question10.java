6caab013113fef3e939046c56104000ee95e9626
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-09 14:13:21 +0530

    4.2 QB - loops in java [for loop Advanced]

---
+++ src/ForloopQB42/Question10.java
@@ -0,0 +1,14 @@
+package ForloopQB42;
+
+public class Question10 {
+	
+	public static void main(String[] args) {
+		
+		int a = 12;
+		
+		for(int i = 0 ; i <= 10 ; i++) {
+			System.out.println(a + "*" + i + "=" + (i*a));
+		}
+	}
+
+}
