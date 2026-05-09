a24743495191c4b11623b954f6065c6d3c73e67f
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-09 12:59:02 +0530

    4.2 QB - loops in java [for loop Advanced]

---
+++ src/ForloopQB42/Question7.java
@@ -0,0 +1,14 @@
+package ForloopQB42;
+
+public class Question7 {
+
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 30; i++) {
+
+			if (i % 3 == 0) {
+				System.out.println(i);
+			}
+		}
+	}
+}
