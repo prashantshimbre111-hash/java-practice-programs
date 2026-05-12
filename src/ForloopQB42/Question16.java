7709d13ac80b950d9b431f629f09fde5a182f3f2
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-12 16:30:09 +0530

    4.2 QB - loops in java [for loop Advanced]

---
+++ src/ForloopQB42/Question16.java
@@ -0,0 +1,17 @@
+package ForloopQB42;
+
+public class Question16 {
+	public static void main(String[] args) {
+		
+		int a = 10;
+		int fact = 1;
+		
+		for (int i = 1 ; i <= 10 ; i++) {
+			
+			fact = fact * i;
+			
+		}
+		System.out.println("factorial number : " + fact);
+	}
+
+}
