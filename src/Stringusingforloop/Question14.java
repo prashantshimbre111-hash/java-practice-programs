51e29a687217f3ff886900da46db82d6171f6e68
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-12 14:04:56 +0530

    5.2 QB  String using for loop

---
+++ src/Stringusingforloop/Question14.java
@@ -0,0 +1,39 @@
+package Stringusingforloop;
+
+public class Question14 {
+	public static void main(String[] args) {
+		
+	
+	
+	String s = "prashant 98754567";
+	
+
+//	for(int i = 0 ; i < s.length() ; i++) {
+//		
+//		char c = s.charAt(i);
+//		
+//		if(Character.isDigit(c)) {
+//			
+//			System.out.println("It is not alphabetic");
+//		}
+//		else {
+//			System.out.println("It is alphabetic");
+//		}
+//	}
+//	Above code is correct but it is printing in loop.
+	
+//	String result = "";
+//	
+//	for(int i = 0 ; i < s.length() ; i++) {
+//		
+//		char c = s.charAt(i);
+//		
+//		if(Character.isAlphabetic(c)) {
+//			result = result + c;
+//		}
+//	}
+//	System.out.println("it is alphabetic : " + result);
+//	
+//	Above code is correct but it is filtering alphabets only.
+	}
+}
