e4e1bdea07e9030c0430e129a1d22bf35c915f58
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-13 14:31:51 +0530

    4.1 QB - loops in java [while loop] 4.1 QB - loops in java [while loop]

---
+++ src/Whileloop41QB/Question1.java
@@ -0,0 +1,13 @@
+package Whileloop41QB;
+
+public class Question1 {
+	public static void main(String[] args) {
+		
+		int i = 0;
+		while(i <= 10) {
+			System.out.println(i);
+			i++;
+		}
+	}
+
+}
