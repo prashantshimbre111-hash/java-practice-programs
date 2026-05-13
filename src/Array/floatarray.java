b8ae5e9a284961cccc3a34dfa207063bd8e929e0
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-13 12:56:47 +0530

    float array implemented

---
+++ src/Array/floatarray.java
@@ -0,0 +1,16 @@
+package Array;
+
+public class floatarray {
+
+		public static void main(String[] args) {
+			
+			float[] s = {122f , 435f , 543f , 44f , 876f , 566f};
+			
+			System.out.println(s[5]);
+			
+			for(int i = 0 ; i < s.length ; i++) {
+				
+				System.out.println(s[i]);
+			}
+		}
+}
