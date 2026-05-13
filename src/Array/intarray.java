ad09b56bd6664b50ebeefae8fbf7a58aca52a341
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-13 13:09:29 +0530

    int array implemented

---
+++ src/Array/intarray.java
@@ -0,0 +1,15 @@
+package Array;
+
+public class intarray {
+	
+	public static void main(String[] args) {
+		
+		int[] l = {12,33,22,44,555,66,777777,};
+		
+		for(int i = 0 ; i < l.length ; i++) {
+			
+			System.out.println(l[i]);
+		}
+	}
+
+}
