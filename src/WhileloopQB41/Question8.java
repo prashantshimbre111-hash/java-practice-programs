485202291d4ec7de98625a1b1add1505500a81e4
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-13 16:01:50 +0530

    . Print the cube of all numbers from 1 to n (user input).

---
+++ src/WhileloopQB41/Question8.java
@@ -0,0 +1,15 @@
+package WhileloopQB41;
+
+public class Question8 {
+	public static void main(String[] args) {
+		
+//		. Print the cube of all numbers from 1 to n (user input).
+
+			int i = 1;
+			while(i <= 10) {
+				System.out.println("cube of " + i + ":" + i*i*i);
+				i++;
+			}
+	}
+
+}
