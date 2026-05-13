b762b8ded72af1f9ebc8caebb74d7c8fca99aaeb
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-13 14:38:59 +0530

    Print all even numbers between 1 to 50.

---
+++ src/Whileloop41QB/Question3.java
@@ -0,0 +1,16 @@
+package Whileloop41QB;
+
+public class Question3 {
+	public static void main(String[] args) {
+		
+//		3. Print all even numbers between 1 to 50.
+
+		int i = 1;
+		while(i <= 50) {
+			
+			System.out.println(i);
+			i++;
+		}
+	}
+
+}
