861ad3c2fc748404e8cd727bff96e3fc3b83e6a1
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-13 12:46:35 +0530

    long array implemented

---
+++ src/Array/longarray.java
@@ -0,0 +1,27 @@
+package Array;
+
+public class longarray {
+	public static void main(String[] args) {
+		
+		long[] s = {99778685, 11223233, 433333456, 332333543, 543333432, 334433566, 665344433};
+		
+		for(int i = 0 ; i < s.length ; i++) {
+			
+			System.out.println(s[i]);
+			
+		}
+		System.out.println("==================================================================");
+		
+		for(int i = s.length-1 ; i >= 0  ; i--) {
+			
+			System.out.println(s[i]);
+			
+			
+				
+				
+			}
+
+		}
+	}
+
+
