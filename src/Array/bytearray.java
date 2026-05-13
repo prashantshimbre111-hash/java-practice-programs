0d2a00f1a8099cf9242e1366a2e274e6e2841c8d
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-13 12:53:56 +0530

    byte array implemented

---
+++ src/Array/bytearray.java
@@ -0,0 +1,17 @@
+package Array;
+
+public class bytearray {
+	
+	public static void main(String[] args) {
+		
+		byte[] s = {12,43,54,66,77,88,99,};
+		
+		System.out.println(s[6]);
+		
+		for(int i = 0 ; i < s.length ; i++) {
+			
+			System.out.println(s[i]);
+		}
+	}
+
+}
