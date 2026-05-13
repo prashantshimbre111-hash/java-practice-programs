d36361b0511848ee735672d96edbb2d568c70eb7
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-13 13:02:05 +0530

    double array implemented

---
+++ src/Array/doublearray.java
@@ -0,0 +1,16 @@
+package Array;
+
+public class doublearray {
+	public static void main(String[] args) {
+		
+	
+	
+	double[] s = {1234.43 , 123.43 , 6677.87 , 77657.76 , 76778.87};
+	
+	for(int i = 0 ; i < s.length ; i++) {
+		
+		System.out.println(s[i]);
+	}
+
+}
+}
