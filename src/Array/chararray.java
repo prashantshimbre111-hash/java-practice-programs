b5511aad5c85e415da52d50311aedd93b4107f8c
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-13 13:05:43 +0530

    char array implemented

---
+++ src/Array/chararray.java
@@ -0,0 +1,14 @@
+package Array;
+
+public class chararray {
+	public static void main(String[] args) {
+		
+		char[] c = {'a' , 'A' , 'f' , 'W' , 'P'};
+		
+		for(int i = 0 ; i < c.length ; i++) {
+			
+			System.out.println(c[i]);
+		}
+	}
+
+}
