58734a0d3b243f1931b53c38fb32202545375faa
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-13 14:03:22 +0530

    reverse digit with for loop implemented

--- src/ForloopQB42/Question17.java
+++ src/ForloopQB42/Question17.java
@@ -1,5 +1,24 @@
 package ForloopQB42;
 
 public class Question17 {
+	public static void main(String[] args) {
+		int n = 1234;
+		
+		int result = 0;
+		
+		for(int i = 0 ; n > 0 ; i++) {
+			
+			int last = n%10;
+			
+			
+			result = result*10 + last;
+			
+			n = n/10;
+
+			
+		}
+		System.out.println(result);
+				
+	}
 
 }
