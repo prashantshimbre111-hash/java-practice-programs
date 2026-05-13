60a03de45c7906bdce7461c9659c4985c5dcafc5
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-13 16:12:11 +0530

    . Print the first n natural numbers.

---
+++ src/WhileloopQB41/Question10.java
@@ -0,0 +1,22 @@
+package WhileloopQB41;
+
+import java.util.Scanner;
+
+public class Question10 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("pleasee enter natural number : ");
+		int n = sc.nextInt();
+		
+		int i = 1;
+		while(i <= n) {
+			System.out.println(i);
+			i++;
+		}
+				
+	}
+
+}
