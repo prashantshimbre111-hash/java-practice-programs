a512dc3b8a721d3db33de3a8429f65ba6ac34153
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-13 12:51:13 +0530

    short array impemented

---
+++ src/Array/shortarray.java
@@ -0,0 +1,15 @@
+package Array;
+
+public class shortarray {
+	public static void main(String[] args) {
+		
+	
+	short[] s = {200 , 55 ,132 , 123 , 143 , 77};
+	
+	for(int i = 0 ; i < s.length ; i++) {
+		
+		System.out.println(s[i]);
+	}
+
+}
+}
