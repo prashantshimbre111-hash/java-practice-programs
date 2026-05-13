b2791b2beec0a47acaeac2357fad074ec401f15b
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-13 14:42:52 +0530

    Print all odd numbers between 1 to 50.

--- src/Whileloop41QB/Question4.java
+++ src/Whileloop41QB/Question4.java
@@ -1,15 +1,15 @@
 package Whileloop41QB;
 
-public class Question3 {
+public class Question4 {
+
 	public static void main(String[] args) {
 
-//		3. Print all even numbers between 1 to 50.
+//		3. Print all odd numbers between 1 to 50.
 
 		int i = 1;
 		while (i <= 50) {
 
-			if (i % 2 == 0) {
-
+			if (i % 2 == 1) {
 				System.out.println(i);
 			}
 			i++;
