2dc8e93a51d2fd2a8d26ef4e43551b55a8bc8991
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-13 15:58:00 +0530

    . Print the square of all numbers from 1 to 10.

---
+++ src/WhileloopQB41/Question7.java
@@ -0,0 +1,16 @@
+package WhileloopQB41;
+
+public class Question7 {
+	public static void main(String[] args) {
+		
+//	. Print the square of all numbers from 1 to 10.
+
+		int i = 1;
+		while(i <= 10) {
+			
+			System.out.println("Square of " + i + ": " + i*i);
+			i++;
+		}
+	}
+
+}
