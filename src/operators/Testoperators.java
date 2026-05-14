628c4d6fc4710bed40f3aa5ca15f7a06ec740883
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-14 11:16:30 +0530

    operators implemented

---
+++ src/operators/Testoperators.java
@@ -0,0 +1,17 @@
+package operators;
+
+public class Testoperators {
+
+	public static void main(String[] args) {
+
+		int a = 12;
+		int b = 98;
+		
+		System.out.println("addition of a and b : " + (a+b));
+		System.out.println("subtraction of a and b : " + (b-a));
+		System.out.println("multiplication of a and b : " + (a*b));
+		System.out.println("division of a and b : " + (b/a));
+		System.out.println("mod of b and a : " + b%a);
+
+	}
+}
