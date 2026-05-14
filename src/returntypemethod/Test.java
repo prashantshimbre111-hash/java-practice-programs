e0fce78f0ef4621f5bd2d80ee861787fb9cb49db
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-14 11:27:19 +0530

    returntype method practice implemented

--- src/returntypemethod/Test.java
+++ src/returntypemethod/Test.java
@@ -11,6 +11,10 @@ public class Test {
 		calculation c = new calculation();
 		int cal = c.sum();
 		System.out.println("addition of a and b of helper class in main  class by using return type method : " + cal);
+		
+		returnmethod r = new returnmethod();
+		int total1 = r.practice();
+		System.out.println(total1);
 
 	}
 
---
+++ src/returntypemethod/returnmethod.java
@@ -0,0 +1,12 @@
+package returntypemethod;
+
+public class returnmethod {
+	
+	public int practice() {
+	int a = 55;
+	int b = 66;
+	int c = a+b;
+	return c;
+	}
+
+}
