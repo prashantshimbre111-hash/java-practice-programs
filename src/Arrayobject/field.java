7f48db31686de31297d8fee3fd97158fef0c85d6
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-14 17:32:13 +0530

    scanner class practice implemented.

---
+++ src/Arrayobject/field.java
@@ -0,0 +1,13 @@
+package Arrayobject;
+
+public class field {
+	
+	public int id;
+	public String fname;
+	public String lname;
+	public String email;
+	public String education;
+	public long mobile;
+	
+
+}
---
+++ src/scanner/practice.java
@@ -0,0 +1,23 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class practice {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("please enter number b : ");
+		int a = sc.nextInt();
+		
+		System.out.println("please enter number b : ");
+		int b = sc.nextInt();
+		
+		int total = a+b;
+		
+		System.out.println(a+b);
+
+	}
+
+}
