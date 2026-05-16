dfa8dc3242d6721f414180c7a570f400fe0a9aa5
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-16 17:57:00 +0530

    Array with encapsulation implemented

---
+++ src/arraywithencapsulation/customer.java
@@ -0,0 +1,57 @@
+package arraywithencapsulation;
+public class customer{
+  
+  private int id;
+  
+  public void setId(int id){
+	  this.id = id;
+  }
+  public int getId(){
+	  return id;
+  }
+  
+  
+ private String name;
+   
+  public void setName(String name){
+	  this.name = name;
+  }
+  public String getName(){
+	  return name;
+  }
+  
+  private String city;
+  
+  public void setCity(String city){
+	  this.city = city;
+  }
+  public String getCity(){
+	  return city;
+  }
+  
+  private String product;
+  
+  public void setProduct(String product){
+	  this.product = product;
+  }
+  public String getProduct(){
+	  return product;
+  }
+	  
+  private int price;
+  
+  public void setPrice(int price){
+	  this.price = price;
+  }
+  public int getPrice(){
+	  return price;
+  }
+  
+  private String date;
+  public void setDate(String date){
+	  this.date = date;
+  }
+  public String getDate(){
+	  return date;
+  }
+}
