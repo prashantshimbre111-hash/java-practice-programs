02c1d50462df2d784b4256f88599f09c1cf497f7
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-16 11:53:57 +0530

    global variable printed using parameterized constructor

---
+++ src/Globalvariable/Test.java
@@ -0,0 +1,9 @@
+package Globalvariable;
+
+public class Test {
+	public static void main(String[] args) {
+		gbprint gb = new gbprint(60);
+		gb.syso();
+	}
+
+}
---
+++ src/Globalvariable/gbprint.java
@@ -0,0 +1,19 @@
+package Globalvariable;
+
+public class gbprint {
+	
+	int a;
+	
+	public gbprint(int a) {
+		this.a = a;
+		
+	}
+	public void syso() {
+		
+		System.out.println(a);
+	}
+	
+
+	}
+
+
