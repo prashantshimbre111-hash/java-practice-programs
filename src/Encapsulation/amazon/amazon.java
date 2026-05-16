cc90d9899d8f21e8cc2916a9e983b09ff5eca53b
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-16 15:39:08 +0530

    encapsulation implemented

---
+++ src/Encapsulation/amazon/amazon.java
@@ -0,0 +1,23 @@
+package Encapsulation.amazon;
+
+public class amazon {
+
+		private String id;
+		private String password;
+		
+		public void setId(String id) {
+			this.id = id;
+			
+		}
+		public String getId() {
+			return id;
+		}
+		public void setPassword(String password) {
+			this.password = password;
+			
+			
+		}
+		public String getPassword() {
+			return password;
+		}
+}
