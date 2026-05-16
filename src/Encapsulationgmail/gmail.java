b4fe5388e48508406bb991c48a71f4614642f0b1
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-16 14:19:13 +0530

    encapsulation implemented

---
+++ src/Encapsulationgmail/gmail.java
@@ -0,0 +1,17 @@
+package Encapsulationgmail;
+
+public class gmail {
+	
+	private String username;
+	private String password;
+	
+	public void setUsername(String username) {
+		
+		this.username = username;
+		
+	}
+	public String getUsername() {
+		return username;
+	}
+
+}
