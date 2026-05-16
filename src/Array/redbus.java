27b660f0b419be5d8ce9cbe265d7f034e25699d3
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-16 15:59:20 +0530

    array class implemented

---
+++ src/Array/redbus.java
@@ -0,0 +1,26 @@
+package Array;
+
+public class redbus{
+ public static void main(String [] args){
+	
+	int[] redbus = new int[5];
+	
+	redbus[0] = 23;
+	redbus[1] = 22;
+	redbus[2] = 20;
+	redbus[3] = 19;
+	redbus[4] = 18;
+	
+	for(int i = 0 ; i < 5 ; i++){
+	
+	System.out.println(redbus[i]);
+	}
+		
+	
+
+ 
+ }
+
+
+
+}
