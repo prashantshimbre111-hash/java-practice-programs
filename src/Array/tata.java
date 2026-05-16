d0cbfa8e52a36b79a70f29263d14015f44fd223b
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-16 16:22:36 +0530

    array implememnted

---
+++ src/Array/tata.java
@@ -0,0 +1,16 @@
+package Array;
+
+public class tata{
+
+	public static void main(String [] args ){
+	
+	int[] tata = {21,43,665,76,77,66,54,33,};
+	
+	for(int i = 0 ; i < 8 ; i++){
+		
+		System.out.println(tata[i]);
+	}
+	}
+	
+	
+	}
