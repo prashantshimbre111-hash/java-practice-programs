0eec9f8af074e97caadae5fa5e2cd1aaa4af2e38
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-06-03 16:01:30 +0530

    polymorphism overriding practice implememnted

---
+++ src/polymorphism/overriding/bank.java
@@ -0,0 +1,15 @@
+package polymorphism.overriding;
+
+public class bank {
+	
+	public void cal(int a, int b) {
+		System.out.println(a+b);
+		
+	}
+	
+	public void cal(int a, int b, int c) {
+		System.out.println(a+b+c);
+		
+	}
+
+}
---
+++ src/polymorphism/overriding/test.java
@@ -0,0 +1,10 @@
+package polymorphism.overriding;
+
+public class test { public static void main(String[] args) {
+	
+	bank b= new bank();
+	b.cal(12, 13, 44);
+}
+	
+
+}
