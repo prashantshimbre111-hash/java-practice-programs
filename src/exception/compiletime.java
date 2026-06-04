c4438048761fae1e090309471367ada24fd540a7
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-06-04 16:36:48 +0530

    compile time exception practice implemented

--- src/exception/compiletime.java
+++ src/exception/compiletime.java
@@ -1,5 +1,21 @@
 package exception;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class compiletime {
+	
+	public static void main(String[] args) {
+		
+	try {	FileReader f = new FileReader("abc.text");
+	}
+	catch(FileNotFoundException e) {
+		System.out.println("file not found");
+		
+	}
+	
+	
+	
+	}
 
 }
