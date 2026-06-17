c2f0ec389adb985798a4273fccbc0ee8e10b7904
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-06-17 22:32:28 +0530

    if else codition basic practice implemented

---
+++ src/conditionalstatement/ifelse.java
@@ -0,0 +1,15 @@
+package conditionalstatement;
+
+public class ifelse {
+	public static void main(String[] args) {
+		int a = 52;
+		
+		if(a%2==0) {
+			System.out.println("it is even number");
+		}
+		else {
+			System.out.println("it is odd number");
+		}
+	}
+
+}
