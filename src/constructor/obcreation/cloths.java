4d712e9b348504876d9901776ce140d972d533f1
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-06-17 21:51:11 +0530

    object creation practice implemented

---
+++ src/constructor/obcreation/cloths.java
@@ -0,0 +1,19 @@
+package constructor.obcreation;
+
+public class cloths {
+	
+	public void getjeans() {
+		
+		System.out.println("hilander navy blue jeans and its size is 32");
+	}
+	
+	String brand = "allen solley white shirt";
+	
+	int price = 749;
+	
+	int size = 42;
+	
+	
+	
+
+}
---
+++ src/constructor/obcreation/flipkart.java
@@ -0,0 +1,18 @@
+package constructor.obcreation;
+
+public class flipkart {
+	
+	public static void main(String[] args) {
+		
+		cloths c = new cloths();
+		System.out.println(c.brand);
+		System.out.println(c.price);
+		System.out.println(c.size);
+		System.out.println("========");
+		c.getjeans();
+		
+		
+		
+	}
+
+}
