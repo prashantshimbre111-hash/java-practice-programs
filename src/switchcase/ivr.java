9383096507ca75592917ed3c091fd0215e222096
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-06-18 13:04:21 +0530

    switch case practice implemented

---
+++ src/switchcase/ivr.java
@@ -0,0 +1,33 @@
+package switchcase;
+
+public class ivr {
+	
+	public static void main(String[] args) {
+		
+		int ivr = 2;
+		
+		switch (ivr) {
+		
+		case 1:
+			System.out.println("balence check");
+			break;
+			
+		case 2:
+			System.out.println("create acc");
+			break;
+			
+		case 3:
+			System.out.println("verify acc");
+			break;
+			
+		default:
+			System.out.println("please enter propper number");
+			
+		
+		
+		
+		
+		}
+	}
+
+}
