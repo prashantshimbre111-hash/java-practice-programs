1308e98eab8846333bb31643d146ddd980288cea
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-06-18 14:32:37 +0530

    new array practice implemented

---
+++ src/Array/test1.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class test1 {
+	
+	public void main(String[] args) {
+		
+		int[] a = new int[5];
+		
+		int[] s = new int[4];
+		
+		String[] b = new String[6];
+		
+		String[] c = new String[5];
+		
+		
+	}
+
+}
