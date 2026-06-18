b37d538a571b30fc00739d38bbeecba5e3be94c8
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-06-18 13:35:30 +0530

    String non static method practice implemented

---
+++ src/String/test.java
@@ -0,0 +1,23 @@
+package String;
+
+public class test {
+	
+	public static void main(String[] args) {
+		
+		String s = "prashant";
+		
+		System.out.println(s.length());
+		System.out.println(s.toUpperCase());
+		System.out.println(s.toLowerCase());
+		System.out.println(s.startsWith("p"));
+		System.out.println(s.charAt(3));
+		System.out.println(s.endsWith("h"));
+		System.out.println(s.contains(s));
+		System.out.println(s.contains("q"));
+		System.out.println(s.indexOf("t"));
+		
+	}
+	
+	
+
+}
