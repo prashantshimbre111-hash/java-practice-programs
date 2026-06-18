bcf1e426ac67aacc8232a784f1f57ab6920ce1d0
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-06-18 11:27:18 +0530

    Grade result with ladder if practice implemented

---
+++ src/nestedifelse/ladderif.java
@@ -0,0 +1,25 @@
+package nestedifelse;
+
+public class ladderif {
+	public static void main(String[] args) {
+		
+		int marks = 40;
+		
+		if(marks >= 90) {
+			System.out.println("A Grade");
+		}
+		else if(marks>=80) {
+			System.out.println("B Grade");
+		}
+		else if(marks >=70) {
+			System.out.println("C Grade");
+		}
+		else if(marks >=60) {
+			System.out.println("D Grade");
+		}
+		else if (marks >=35) {
+			System.out.println("E Grade");
+		}
+	}
+
+}
