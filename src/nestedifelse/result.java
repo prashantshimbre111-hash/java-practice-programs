a92f7678111ce2b084bbb35f876dcb2d893e9d86
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-06-18 11:20:09 +0530

    nested if with more condition practice implemented

---
+++ src/nestedifelse/result.java
@@ -0,0 +1,22 @@
+package nestedifelse;
+
+public class result {
+	
+	public static void main(String[] args) {
+		
+		int marks = 85;
+		
+		if(marks >= 35) {
+			if(marks >=75) {
+				System.out.println("pass with distinction");
+			}
+			else {
+				System.out.println("pass");
+			}
+		}
+		else {
+			System.out.println("fail");
+		}
+	}
+
+}
