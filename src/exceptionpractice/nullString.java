9f478189f493ea64e7dd481d4321e1bb8606775c
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-06-20 12:22:57 +0530

    NullPointerException practice implemented

---
+++ src/exceptionpractice/nullString.java
@@ -0,0 +1,17 @@
+package exceptionpractice;
+
+public class nullString {
+	
+	public static void main(String[]args) {
+		
+		try{String s = null;
+		
+		System.out.println(s.length());
+		}
+		catch (NullPointerException e) {
+			System.out.println("NullPointerException found");
+		}
+		System.out.println("pragram Ended");
+	}
+
+}
